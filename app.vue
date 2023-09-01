d6d4feec6c7ce549767389125d62cbf230d1ba64
FireStreaker2 <michaelccc2019@outlook.com>
2023-09-01 09:35:42 -0700

    loading animation

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
 	<div class="body" ref="container">
+		<Head>
+			<Title>NuxtProxy</Title>
+		</Head>
 		<div class="navbar" id="navbar">
 			<button @click="fullScreen" id="fullscreen">
 				<svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -11,14 +14,26 @@
 					/>
 				</svg>
 			</button>
-			<h1>{{ iframeTitle }}</h1>
+			<h1>{{ title }}</h1>
 			<button @click="close">X</button>
 		</div>
-		<div>
+		<div id="homeContainer">
 			<h1>NuxtProxy</h1>
 			<form @submit.prevent="submitForm">
 				<input v-model="query" placeholder="Search Now" />
 			</form>
+		</div>
+		<div class="center" id="loading">
+			<div class="wave"></div>
+			<div class="wave"></div>
+			<div class="wave"></div>
+			<div class="wave"></div>
+			<div class="wave"></div>
+			<div class="wave"></div>
+			<div class="wave"></div>
+			<div class="wave"></div>
+			<div class="wave"></div>
+			<div class="wave"></div>
 		</div>
 	</div>
 </template>
@@ -29,18 +44,12 @@ export default {
 		return {
 			query: "",
 			prefix: "/~/",
-			title: "NuxtProxy",
-      iframeTitle: "Loading...",
-		};
-	},
-	head() {
-		return {
-			title: this.title,
+			title: "Loading...",
 		};
 	},
 	created() {
 		if (process.browser && "serviceWorker" in navigator) {
-			const registerSW = async () => {
+			(async () => {
 				const registrations = await navigator.serviceWorker.getRegistrations();
 				for (const registration of registrations) {
 					await registration
@@ -67,8 +76,7 @@ export default {
 					.catch((error) => {
 						console.log(`[INFO] Service Worker Failed to Register: ${error}`);
 					});
-			};
-			registerSW();
+			})();
 		}
 	},
 	methods: {
@@ -86,6 +94,8 @@ export default {
 
 				this.loadLink(this.query);
 				document.getElementById("navbar").style.display = "flex";
+				document.getElementById("homeContainer").style.display = "none";
+				document.getElementById("loading").style.display = "flex";
 			} else {
 				alert("Please enter a value.");
 			}
@@ -121,9 +131,9 @@ export default {
 				if (container) {
 					container.appendChild(iframe);
 
-          iframe.onload = () => {
-            this.iframeTitle = iframe.contentDocument.title;
-          }
+					iframe.onload = () => {
+						this.title = iframe.contentDocument.title;
+					};
 				}
 			}
 		},
@@ -163,6 +173,8 @@ export default {
 		close() {
 			document.getElementById("navbar").style.display = "none";
 			document.getElementsByTagName("iframe")[0].remove();
+			document.getElementById("homeContainer").style.display = "flex";
+			document.getElementById("loading").style.display = "none";
 		},
 	},
 };
@@ -184,6 +196,32 @@ body {
 	justify-content: center;
 	align-items: center;
 	background: linear-gradient(to right, #4fc08d, #13a4ff);
+}
+
+#homeContainer {
+	text-align: center;
+	flex-direction: column;
+}
+
+input {
+	background-color: #656464;
+	text-align: center;
+	border: none;
+	border-radius: 5px;
+	color: #01fd83;
+}
+
+input::placeholder,
+h1 {
+	color: #aac8e4;
+	transition: color 0.3s ease;
+}
+
+input:hover::placeholder,
+input:focus::placeholder,
+h1:hover {
+	color: #01fd83;
+	border: none;
 }
 
 iframe {
@@ -223,5 +261,60 @@ iframe {
 #fullscreen svg {
 	width: 100%;
 	height: 100%;
+}
+
+.center {
+	position: absolute;
+	height: 100vh;
+	display: none;
+	justify-content: center;
+	align-items: center;
+}
+.wave {
+	width: 5px;
+	height: 100px;
+	background: linear-gradient(45deg, cyan, #fff);
+	margin: 10px;
+	animation: wave 1s linear infinite;
+	border-radius: 20px;
+}
+.wave:nth-child(2) {
+	animation-delay: 0.1s;
+}
+.wave:nth-child(3) {
+	animation-delay: 0.2s;
+}
+.wave:nth-child(4) {
+	animation-delay: 0.3s;
+}
+.wave:nth-child(5) {
+	animation-delay: 0.4s;
+}
+.wave:nth-child(6) {
+	animation-delay: 0.5s;
+}
+.wave:nth-child(7) {
+	animation-delay: 0.6s;
+}
+.wave:nth-child(8) {
+	animation-delay: 0.7s;
+}
+.wave:nth-child(9) {
+	animation-delay: 0.8s;
+}
+.wave:nth-child(10) {
+	animation-delay: 0.9s;
+}
+
+@keyframes wave {
+	0% {
+		transform: scale(0);
+	}
+	50% {
+		transform: scale(1);
+	}
+	100% {
+		transform: scale(0);
+	}
 }
 </style>
